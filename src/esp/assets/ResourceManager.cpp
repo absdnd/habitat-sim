ac9e764a590a455f91b2c0f07b65b6e2f76958cd
sanchitgarg <sanchit.garg0526@gmail.com>
2022-04-05 17:40:25 -0700

    Update git submodule

--- src/esp/assets/ResourceManager.cpp
+++ src/esp/assets/ResourceManager.cpp
@@ -2943,7 +2943,8 @@ void ResourceManager::joinSemanticHierarchy(
       return;
     }
 
-    const std::vector<Mn::Vector3>& vertices = meshData->getVertexBufferObjectCPU();
+    const std::vector<Mn::Vector3>& vertices =
+        meshData->getVertexBufferObjectCPU();
     const std::vector<uint32_t>& indices = meshData->getIndexBufferObjectCPU();
     const std::vector<uint16_t>& objectIds =
         meshData->getObjectIdsBufferObjectCPU();
