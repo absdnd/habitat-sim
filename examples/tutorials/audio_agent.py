804e31d0dad50c4cb2196c4a03765d83ab4fbd51
sanchitgarg <sanchit.garg0526@gmail.com>
2022-04-26 12:47:21 -0700

    updated api name

--- examples/tutorials/audio_agent.py
+++ examples/tutorials/audio_agent.py
@@ -69,6 +69,9 @@ def main():
     # set audio source location, no need to set the agent location, will be set implicitly
     audio_sensor.setAudioSourceTransform(np.array([3.1035, 1.57245, -4.15972]))
 
+    # optionally, set the audio materials json
+    audio_sensor.setAudioMaterialsJSON("/home/sangarg/fbsource/arvr/libraries/audio/AudioSDK/Research/Source/TimeMachine/materials_info.json")
+
     # run the simulation, currently only 1 iteration is run
     for i in range(1):
         print(i)
